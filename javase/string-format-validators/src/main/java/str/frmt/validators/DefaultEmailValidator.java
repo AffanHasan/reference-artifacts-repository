3a51aef005e7c659f4fec8715e69370e621468dc
Affan Hasan <affanhasan88@gmail.com>
2015-03-08 02:00:08 +0500

    javase/string-format-validators project became more matured & it contains more mature email validation funcitonality

--- javase/string-format-validators/src/main/java/str/frmt/validators/DefaultEmailValidator.java
+++ javase/string-format-validators/src/main/java/str/frmt/validators/DefaultEmailValidator.java
@@ -4,11 +4,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created by root on 3/5/15.
+ * Created by Affan Hasan on 3/5/15.
  */
 class DefaultEmailValidator implements EmailValidator{
 
-    private final String _regex = "[a-z]+@[a-z]+.com";
+    private final String _regex =
+            "(\\Q(\\E.*\\Q)\\E)?[0-9]*[a-zA-Z]+[0-9]*\\Q.\\E?[a-zA-Z]+[0-9]*(\\Q(\\E.*\\Q)\\E)?@[a-z]+.com";
 
     private final Pattern _pattern = Pattern.compile(_regex);
 
@@ -18,7 +19,7 @@ class DefaultEmailValidator implements EmailValidator{
             throw new IllegalArgumentException();
 
         return _pattern.matcher(email).matches();
-}
+    }
 
     @Override
     public String getRegex() {
--- javase/string-format-validators/src/test/java/str/frmt/validators/EmailValidatorTest.java
+++ javase/string-format-validators/src/test/java/str/frmt/validators/EmailValidatorTest.java
@@ -8,9 +8,13 @@ public class EmailValidatorTest {
     private final EmailValidator _emailValidatorFixture = Factories.EmailValidatorFactory.getInstance();
 
     @Test
+    public void must_return_regex_as_string(){
+        Assert.assertTrue(_emailValidatorFixture.getRegex() instanceof String);
+    }
+
+    @Test
     public void plain_email_test(){
-        String email = "abcxyz@abcxyz.com";
-        Assert.assertTrue(_emailValidatorFixture.isEmailValid(email));
+        Assert.assertTrue(_emailValidatorFixture.isEmailValid("abcxyz@abcxyz.com"));
     }
 
     @Test
@@ -23,8 +27,186 @@ public class EmailValidatorTest {
         Assert.fail();
     }
 
+    /**
+     * johny.depp@carribean.com
+     */
     @Test
-    public void must_return_regex_as_string(){
-        Assert.assertTrue(_emailValidatorFixture.getRegex() instanceof String);
+    public void validate_single_dot_in_between_the_local_part_is_valid(){
+        Assert.assertTrue(_emailValidatorFixture.isEmailValid("johny.depp@carribean.com"));
+    }
+
+    /**
+     * johny..depp@carribean.com
+     */
+    @Test
+    public void validate_two_consecutive_dots_in_local_part_is_invalid(){
+        Assert.assertFalse(_emailValidatorFixture.isEmailValid("johny..depp@carribean.com"));
+    }
+
+    /**
+     * .johnydepp@carribean.com
+     */
+    @Test
+    public void validate_single_dot_at_the_beginning_of_local_part_is_invalid(){
+        Assert.assertFalse(_emailValidatorFixture.isEmailValid(".johnydepp@carribean.com"));
+    }
+
+    /**
+     * ..johnydepp@carribean.com
+     */
+    @Test
+    public void validate_two_single_dots_at_the_beginning_of_local_part_is_invalid(){
+        Assert.assertFalse(_emailValidatorFixture.isEmailValid(".johnydepp@carribean.com"));
+    }
+
+    /**
+     * johnydepp.@carribean.com
+     */
+    @Test
+    public void validate_single_dot_at_the_end_of_local_part_is_invalid(){
+        Assert.assertFalse(_emailValidatorFixture.isEmailValid("johnydepp.@carribean.com"));
+    }
+
+    /**
+     * johnydepp..@carribean.com
+     */
+    @Test
+    public void validate_2_consecutive_dots_at_the_end_of_local_part_is_invalid(){
+        Assert.assertFalse(_emailValidatorFixture.isEmailValid("johnydepp..@carribean.com"));
+    }
+
+    /**
+     * JOHNYDEPP@carribean.com
+     */
+    @Test
+    public void validate_UPPER_CASE_LETTERS_A_TO_Z_are_valid_in_local_part(){
+        Assert.assertTrue(_emailValidatorFixture.isEmailValid("JOHNYDEPP@carribean.com"));
+    }
+
+    /**
+     * JOHNYdepp@carribean.com
+     */
+    @Test
+    public void validate_UPPER_CASE_plus_lower_case_letters_a_to_z_are_valid_in_local_part(){
+        Assert.assertTrue(_emailValidatorFixture.isEmailValid("JOHNYdepp@carribean.com"));
+    }
+
+    /**
+     * DEPPjohny@carribean.com
+     */
+    @Test
+    public void validate_UPPER_CASE_letters_a_to_z_are_valid_at_the_start_of_local_part(){
+        Assert.assertTrue(_emailValidatorFixture.isEmailValid("DEPPjohny@carribean.com"));
+    }
+
+    /**
+     * 123johnydepp@carribean.com
+     */
+    @Test
+    public void validate_local_part_starting_from_0_to_9_is_valid(){
+        for(int i = 0; i <=9; i++){
+            Assert.assertTrue(_emailValidatorFixture.isEmailValid(i + "johnydepp@carribean.com"));
+        }
+    }
+
+    /**
+     * johny123Depp@carribean.com
+     */
+    @Test
+    public void validate_local_part_containing_digits_from_0_to_9_is_valid(){
+        for(int i = 0; i <=9; i++){
+            Assert.assertTrue(_emailValidatorFixture.isEmailValid("johny" + i + "Depp@carribean.com"));
+        }
+    }
+
+    /**
+     * johny123Depp123@carribean.com
+     */
+    @Test
+    public void validate_local_part_ending_with_digits_from_0_to_9_is_valid(){
+        for(int i=0; i<=9; i++){
+            Assert.assertTrue(_emailValidatorFixture.isEmailValid("johny123Depp" + i + "@carribean.com"));
+        }
+    }
+
+    /**
+     * @johnydepp@carribean.com
+     */
+    @Test
+    public void validate_local_part_starting_with_at_the_rate_character_is_in_valid(){
+        Assert.assertFalse(_emailValidatorFixture.isEmailValid("@johnydepp@carribean.com"));
+    }
+
+    /**
+     * johny@depp@carribean.com
+     */
+    @Test
+    public void validate_local_part_containing_at_the_rate_character_is_in_valid(){
+        Assert.assertFalse(_emailValidatorFixture.isEmailValid("johny@depp@carribean.com"));
+    }
+
+    /**
+     * johnydepp@@carribean.com
+     */
+    @Test
+    public void validate_local_part_ending_with_at_the_rate_character_is_in_valid(){
+        Assert.assertFalse(_emailValidatorFixture.isEmailValid("johnydepp@@carribean.com"));
+    }
+
+    /**
+     * johny:depp@carribean.com
+     */
+    @Test
+    public void validate_local_part_containing_colon_character_is_in_valid(){
+        Assert.assertFalse(_emailValidatorFixture.isEmailValid("johny:depp@carribean.com"));
+    }
+
+    /**
+     * :johnydepp@carribean.com
+     */
+    @Test
+    public void validate_local_part_starting_with_colon_character_is_in_valid(){
+        Assert.assertFalse(_emailValidatorFixture.isEmailValid(":johnydepp@carribean.com"));
+    }
+
+    /**
+     * johnydepp:@carribean.com
+     */
+    @Test
+    public void validate_local_part_ending_with_colon_character_is_in_valid(){
+        Assert.assertFalse(_emailValidatorFixture.isEmailValid("johnydepp:@carribean.com"));
+    }
+
+    /**
+     * (office address)johnydepp@carribean.com
+     */
+    @Test
+    public void validate_local_part_starting_with_comments_is_valid(){
+        Assert.assertTrue(_emailValidatorFixture.isEmailValid("(office address)johnydepp@carribean.com"));
+    }
+
+    /**
+     * johnydepp(office address)@carribean.com
+     */
+    @Test
+    public void validate_local_part_ending_with_comments_is_valid(){
+        Assert.assertTrue(_emailValidatorFixture.isEmailValid("johnydepp(office address)@carribean.com"));
+    }
+
+    /**
+     * ()johnydepp@carribean.com
+     */
+    @Test
+    public void validate_local_part_starting_with_and_ending_with_empty_comments_is_valid(){
+        Assert.assertTrue(_emailValidatorFixture.isEmailValid("()johnydepp@carribean.com"));
+        Assert.assertTrue(_emailValidatorFixture.isEmailValid("johnydepp()@carribean.com"));
+    }
+
+    /**
+     * johny(office address)depp@carribean.com
+     */
+    @Test
+    public void validate_local_part_containing_comments_is_in_valid(){
+        Assert.assertFalse(_emailValidatorFixture.isEmailValid("johny(office address)depp@carribean.com"));
     }
 }
