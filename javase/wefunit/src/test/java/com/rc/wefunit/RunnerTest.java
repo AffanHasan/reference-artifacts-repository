cb7325ebf0a005337839b49ffc977f349f48d8de
Affan Hasan <affanhasan88@gmail.com>
2015-03-24 22:27:15 +0500

    Added more test cases

--- javase/wefunit/src/test/java/com/rc/wefunit/RunnerTest.java
+++ javase/wefunit/src/test/java/com/rc/wefunit/RunnerTest.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.lang.reflect.Method;
 import java.nio.file.FileSystem;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Hashtable;
 
 /**
@@ -20,11 +21,13 @@ public class RunnerTest {
 
     private final Runner _runner = Factories.RunnerFactory.getInstance();
 
+    private final String _webInfDirPath = "samplewefproject/WebContent/WEB-INF";
+
     @Test
     public void web_INF_directory_path_not_null(@Mocked SystemProperties systemProperties){
         new Expectations(){
             {
-                SystemProperties.getWebInfDir(); result = "resources/WEB-INF";
+                SystemProperties.getWebInfDir(); result = _webInfDirPath;
             }
         };
         String path = _runner.getWebInfDirPath();
@@ -35,7 +38,7 @@ public class RunnerTest {
     public void web_INF_directory_path_web_inf_as_last_directory(@Mocked SystemProperties systemProperties){
         new Expectations(){
             {
-                SystemProperties.getWebInfDir(); result = "resources/WEB-INF";
+                SystemProperties.getWebInfDir(); result = _webInfDirPath;
             }
         };
         String path = _runner.getWebInfDirPath();
@@ -46,7 +49,7 @@ public class RunnerTest {
     public void web_INF_directory_path_verify_that_this_is_a_wef_project(@Mocked SystemProperties systemProperties){
         new Expectations(){
             {
-                SystemProperties.getWebInfDir(); result = "resources/WEB-INF";
+                SystemProperties.getWebInfDir(); result = _webInfDirPath;
             }
         };
         String path = _runner.getWebInfDirPath();
@@ -65,4 +68,36 @@ public class RunnerTest {
         Assert.assertTrue(containsModelFolder);//Must contain "models" folder
         Assert.assertTrue(containsWorkFolder);//Must contain "work" folder
     }
+
+    @Test
+    public void web_INF_directory_path_if_this_is_not_a_wef_project_then_throw_illegal_state_exception
+            (@Mocked SystemProperties systemProperties, @Mocked File file){
+
+        new Expectations(){
+            {
+                SystemProperties.getWebInfDir(); result = _webInfDirPath;
+                File f = new File("");
+                f.listFiles(); result = new ArrayList<File>();
+            }
+        };
+        String path = _runner.getWebInfDirPath();
+        Assert.assertTrue(file.isDirectory());
+        boolean containsModelFolder = false;
+        boolean containsWorkFolder = false;
+        for( File item : file.listFiles() ){
+            if(item.getName().equals("models") && item.isDirectory()){
+                containsModelFolder = true;
+            }
+            if(item.getName().equals("work") && item.isDirectory()){
+                containsWorkFolder = true;
+            }
+        }
+        Assert.assertFalse(containsModelFolder);//Must not contain "models" folder
+        Assert.assertFalse(containsWorkFolder);//Must not contain "work" folder
+    }
+
+//    @Test
+//    public void (){
+//
+//    }
 }
